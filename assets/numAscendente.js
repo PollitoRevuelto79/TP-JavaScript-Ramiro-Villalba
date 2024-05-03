240f5d706171e1f48f7fb94b4d906f9b347564fa
Iván Ramiro Villalba <ramiroivan79@hotmail.com>
2024-05-03 16:05:23 -0300

    Inicio del trabajo en numAscendente.js

---
+++ assets/numAscendente.js
@@ -0,0 +1,10 @@
+let numbUno = parseInt(prompt('Ingresa un número.'));
+let numbDos = parseInt(prompt('Ingresa un número.'));
+let numbTres = parseInt(prompt('Ingresa un número.'));
+
+if (numbUno>numbDos && numbUno>numbTres) {
+    if (numbDos>numbTres && numbDos>numbTres)
+        console.log(numbUno,numbDos,numbTres)
+} else {
+    
+}
