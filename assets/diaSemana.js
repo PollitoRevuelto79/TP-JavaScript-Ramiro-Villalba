f3043ea8ca7103aecd69bb318945136223bb5d65
Iván Ramiro Villalba <ramiroivan79@hotmail.com>
2024-05-03 13:52:56 -0300

    Se agrego un If-Else que engloba el switch

--- assets/diaSemana.js
+++ assets/diaSemana.js
@@ -1,29 +1,35 @@
 let dia = parseInt(prompt('Ingrese un numero entre el 1 y el 7'));
 
-switch (dia) {
-    case 1:
-        console.log('Lunes')
-        break;
-    case 2:
-        console.log('Martes')
-        break;
-    case 3:
-        console.log('Miercoles')
-        break;
-    case 4:
-        console.log('Jueves')
-        break;
-    case 5:
-        console.log('Viernes')
-        break;
-    case 6:
-        console.log('Sabado')
-    case 7:
-        console.log ('Domingo')
-    case dia != Number:
-        console.log('El caracter ingresado no es valido, por favor ingrese un número')
-        break;
-    default:
-        console.log ('El número ingresado no es valido, pruebe ingresando otro')
-        break;
+if (dia<7) {
+    switch (dia) {
+        case 1:
+            console.log('Lunes')
+            break;
+        case 2:
+            console.log('Martes')
+            break;
+        case 3:
+            console.log('Miercoles')
+            break;
+        case 4:
+            console.log('Jueves')
+            break;
+        case 5:
+            console.log('Viernes')
+            break;
+        case 6:
+            console.log('Sabado')
+            break;
+        case 7:
+            console.log ('Domingo')
+            break;
+        case (8 > dia):
+            console.log('El número ingresado es mayor a 7, ingrese un número entre el 1 y el 7')
+            break;
+        default:
+            console.log ('El número o caracter ingresado no es valido, pruebe con un numero entre el 1 y el 7')
+            break;
+    }
+}else {
+    console.log('El número ingresado es mayor a 7, ingrese un número entre el 1 y el 7')
 }
