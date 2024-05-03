12033784558051b8427126357664da86a6b2510c
Iván Ramiro Villalba <ramiroivan79@hotmail.com>
2024-05-03 16:40:35 -0300

    Se completo una estructura de if-else, hay que arreglar

--- assets/numAscendente.js
+++ assets/numAscendente.js
@@ -1,10 +1,14 @@
-let numbUno = parseInt(prompt('Ingresa un número.'));
-let numbDos = parseInt(prompt('Ingresa un número.'));
-let numbTres = parseInt(prompt('Ingresa un número.'));
+let numbUno = parseInt(prompt('Ingresa un primer número.'));
+let numbDos = parseInt(prompt('Ingresa un segundo número.'));
+let numbTres = parseInt(prompt('Ingresa un tercer número.'));
 
 if (numbUno>numbDos && numbUno>numbTres) {
-    if (numbDos>numbTres && numbDos>numbTres)
+    if (numbDos>numbTres)
         console.log(numbUno,numbDos,numbTres)
-} else {
-    
+} else if (numbDos>numbUno && numbDos>numbTres) {
+    if (numbUno>numbTres)
+        console.log(numbDos,numbUno,numbTres)
+} else if (numbTres>numbUno && numbTres>numbDos) {
+    if (numbUno>numbDos)
+        console.log(numbTres,numbUno,numbDos)
 }
