fb7badc423dda2b27ddf42c1e456c9441c0b4567
Iván Ramiro Villalba <ramiroivan79@hotmail.com>
2024-05-06 09:24:48 -0300

    Se agregó condicional de solo numeros a numAscendente

--- assets/numAscendente.js
+++ assets/numAscendente.js
@@ -2,27 +2,34 @@ let numbUno = parseInt(prompt('Ingrese un primer número'));
 let numbDos = parseInt(prompt('Ingrese un segundo número'));
 let numbTres = parseInt(prompt('Ingrese un tercer número'));
 
-if (numbUno === numbDos) {
-    if (numbUno === numbTres)
-    console.log ('Los tres numeros son iguales.')
+
+if (isNaN(numbUno) || isNaN(numbDos) || isNaN(numbTres)) {
+    console.log ('Por favor, solo ingrese valores númericos.')
 } else {
-    if (numbUno <= numbDos && numbUno <= numbTres) {
-        if (numbDos <= numbTres) {
-            console.log(numbUno, numbDos, numbTres);
-        } else {
-            console.log(numbUno, numbTres, numbDos);
-        }
-    } else if (numbDos <= numbUno && numbDos <= numbTres) {
-        if (numbUno <= numbTres) {
-            console.log(numbDos, numbUno, numbTres);
-        } else {
-            console.log(numbDos, numbTres, numbUno);
-        }
+    if (numbUno === numbDos) {
+        if (numbUno === numbTres)
+        console.log ('Los tres numeros son iguales.')
     } else {
-        if (numbUno <= numbDos) {
-            console.log(numbTres, numbUno, numbDos);
+        if (numbUno <= numbDos && numbUno <= numbTres) {
+            if (numbDos <= numbTres) {
+                console.log(numbUno, numbDos, numbTres);
+            } else {
+                console.log(numbUno, numbTres, numbDos);
+            }
+        } else if (numbDos <= numbUno && numbDos <= numbTres) {
+            if (numbUno <= numbTres) {
+                console.log(numbDos, numbUno, numbTres);
+            } else {
+                console.log(numbDos, numbTres, numbUno);
+            }
         } else {
-            console.log(numbTres, numbDos, numbUno);
+            if (numbUno <= numbDos) {
+                console.log(numbTres, numbUno, numbDos);
+            } else {
+                console.log(numbTres, numbDos, numbUno);
+            }
         }
     }
+    
 }
+
